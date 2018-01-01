c969001b6eac8b70d06d04120b0f25706cead32e
Wes Basinger <wbasinger@villagetechschools.org>
2018-01-01 20:47:38 +0000

    Add CORS support.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from graphene import ObjectType, String, Schema
 from flask_graphql import GraphQLView
+from flask_cors import CORS
+
 
 from schema import schema
 
 view_func = GraphQLView.as_view('graphql', schema=schema, graphiql=True)
 
 app = Flask(__name__)
+CORS(app)
 app.add_url_rule('/', view_func=view_func)
 
 if __name__ == '__main__':
